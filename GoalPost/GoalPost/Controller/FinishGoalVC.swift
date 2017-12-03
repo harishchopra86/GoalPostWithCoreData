135da8ec95242d448228ea1b6741c99999ba0089
harish <harishchopra86@gmail.com>
2017-12-02 22:46:42 -0500

    finish goal vc bug fixes

--- GoalPost/GoalPost/Controller/FinishGoalVC.swift
+++ GoalPost/GoalPost/Controller/FinishGoalVC.swift
@@ -33,21 +33,12 @@ class FinishGoalVC: UIViewController, UITextFieldDelegate {
         
     }
     @IBAction func backBtnTapped(_ sender: Any) {
-        
+        dismissDetail()
         
     }
     
     
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
